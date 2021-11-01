910d977227a049cb64188ccd762ffab0a4139708
Relativelie <sopio1992@gmail.com>
2021-11-01 12:08:07 +0300

    initial commit

---
+++ script.js
@@ -0,0 +1,49 @@
+
+const professionText = "ior front-end developer";
+
+let profession = document.querySelector(".profession p")
+
+showProfession();
+function showProfession() {
+    document.querySelector(".fishPosition").classList.add("fishMove")
+    let i=0;
+        let myInterval = setInterval(()=> {
+            profession.textContent += professionText[i];
+            i++;
+            if (i===professionText.length) {
+                clearInterval(myInterval)
+            }
+        }, 140);
+    }
+
+
+document.querySelector(".fishPosition").onanimationend = () => {
+    if ((Object.values(document.querySelector(".fishPosition").classList).indexOf("fishMove") != -1)) {
+        document.querySelector(".fishPosition").style.right = "-300px";
+        document.querySelector(".fishPosition").classList.remove("fishMove");
+        fishing() 
+    }
+};
+
+function fishing() {
+    document.querySelector(".circleForFishThing").classList.add("fishing");
+    let myTimeout = setTimeout(() => {
+        document.querySelector(".fishPosition").classList.add("caughtFish");
+        document.querySelector(".myFish").classList.add("scaryFish");
+        document.querySelector(".fishPosition").style.top = "-500px";
+    }, 800)
+    
+}
+
+document.querySelector(".circleForFishThing").onanimationend = () => {
+    document.querySelector(".circleForFishThing").classList.remove("fishing");
+    document.querySelector(".fishPosition").classList.remove("caughtFish");
+    document.querySelector(".myFish").classList.remove("scaryFish");
+};
+
+let myVideos = [
+    "siteOne.mp4",
+    "siteThree.mp4",
+    "siteTwo.mp4"
+
+]
