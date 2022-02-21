740344c957af4efc4927a12f94fb2f953aa78837
Relativelie <sopio1992@gmail.com>
2022-02-21 11:36:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,36 +1,24 @@
-
 const professionText = "ior front-end developer";
-
-let profession = document.querySelector(".profession p")
-
+let profession = document.querySelector(".profession p");
 window.addEventListener("load", showProfession)
 
 function showProfession() {
     document.querySelector(".waiting").style.display = "none";
     document.querySelector(".fishPosition").classList.add("fishMove")
-    let i=0;
-        let myInterval = setInterval(()=> {
-            profession.textContent += professionText[i];
-            i++;
-            if (i===professionText.length) {
-                clearInterval(myInterval)
-            }
-        }, 140);
-    }
-
+    let i = 0;
+    let myInterval = setInterval(() => {
+        profession.textContent += professionText[i];
+        i++;
+        i === professionText.length && clearInterval(myInterval);
+    }, 140);
+}
 
 document.querySelector(".fishPosition").onanimationend = () => {
-    let mySize = window.matchMedia('(max-width: 500px)');
     if ((Object.values(document.querySelector(".fishPosition").classList).indexOf("fishMove") != -1)) {
-        if (mySize.matches) {
-            document.querySelector(".fishPosition").style.right = "-150px";
-        }
-
-        else {
+        window.matchMedia('(max-width: 500px)').matches ? document.querySelector(".fishPosition").style.right = "-150px" :
             document.querySelector(".fishPosition").style.right = "-300px";
-        }
         document.querySelector(".fishPosition").classList.remove("fishMove");
-        fishing() 
+        fishing()
     }
 };
 
@@ -40,8 +28,8 @@ function fishing() {
         document.querySelector(".fishPosition").classList.add("caughtFish");
         document.querySelector(".myFish").classList.add("scaryFish");
         document.querySelector(".fishPosition").style.top = "-500px";
+        clearTimeout(myTimeout)
     }, 800)
-    
 }
 
 document.querySelector(".circleForFishThing").onanimationend = () => {
@@ -50,17 +38,12 @@ document.querySelector(".circleForFishThing").onanimationend = () => {
     document.querySelector(".myFish").classList.remove("scaryFish");
 };
 
-
-
 let portfolio = document.querySelector(".portfolio")
 function showPortfolio() {
     portfolio.style.marginTop = "20px";
-    }
-    
-
-
+}
 let observer = new IntersectionObserver(function (e) {
-    if (e[0].isIntersecting === true) {
+    if (e[0].isIntersecting) {
         showPortfolio();
         observer.disconnect();
     }
